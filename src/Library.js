77fc2abfb2167c5d197de7c99bfe5535c079166c
Shane Misra <shane.misra@gmail.com>
2022-12-01 14:59:28 -0700

    Add shelves to a newly constructed library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,13 @@
 
+function createLibrary (name) {
+    var library = {};
+    library.name = name;
+    library.shelves = {fantasy: [] , fiction : [], nonFiction: []}
+    return library;
+}
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  var character = {};
+  character.name = name;
+  character.age = age;
+  character.pronouns = pronouns;
+  return character
+}
+function saveReview (reviewText, reviews){
+for (i = 0; i < reviews.length+1; i++ ) {
+  if (reviewText === reviews[i]) {
+    return reviews
+  } else {
+  reviews.push(reviewText);
+  return reviews;
+}
+}
+}
+
+function calculatePageCount(bookTitle) {
+  var length;
+  length =+ 20* bookTitle.length;
+  return length;
+}
+
+function writeBook (title, character, genre) {
+  var book = {};
+  book.title = title;
+  book.mainCharacter = character;
+  book.pageCount = calculatePageCount(title);
+  book.genre = genre;
+  return book;
+}
+function editBook (bookTitle) {
+  bookTitle.pageCount = (bookTitle.pageCount *.75)
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
